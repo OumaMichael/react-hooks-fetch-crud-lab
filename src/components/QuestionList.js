4f3125c013428896489d1c8d1dd16550ef38d990
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-16 15:01:40 +0300

    Update QuestionList.js

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -12,49 +12,17 @@ function QuestionList() {
       .catch((error) => console.log(error));
   }, []);
 
-  const handleAddQuestion = (newQuestion) => {
-    setQuestions([...questions, newQuestion]);
-  };
-
   const handleDelete = (id) => {
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: 'DELETE',
-    })
-      .then(() => {
-        setQuestions(questions.filter((question) => question.id !== id));
-      })
-      .catch(error => console.log(error));
-  };
-
-  const handleUpdateCorrectAnswer = (id, correctIndex) => {
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ correctIndex: correctIndex }),
-    })
-      .then((response) => response.json())
-      .then((updatedQuestion) => {
-        setQuestions(questions.map((question) =>
-          question.id === id ? updatedQuestion : question
-        ));
-      })
-      .catch(error => console.log(error));
+    setQuestions(questions.filter((question) => question.id !== id));
   };
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <NewQuestionForm onAddQuestion={handleAddQuestion} />
+      <NewQuestionForm />
       <ul>
         {questions.map((question) => (
-          <QuestionItem
-            key={question.id}
-            question={question}
-            onDelete={handleDelete}
-            onUpdateCorrectAnswer={handleUpdateCorrectAnswer}
-          />
+          <QuestionItem key={question.id} question={question} onDelete={handleDelete} />
         ))}
       </ul>
     </section>
